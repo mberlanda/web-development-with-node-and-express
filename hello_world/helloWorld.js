dda22cf8f1f83e374bfd7f056c2239de1d366c8a
Mauro Berlanda <m.berlanda@fit2you.it>
2016-08-24 16:07:49 +0200

    ch02 Getting Started with Node | Serving Static Resources

--- hello_world/helloWorld.js
+++ hello_world/helloWorld.js
@@ -1,4 +1,19 @@
 var http = require('http');
+var fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+  if(!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, function(err, data){
+    if(err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain'});
+      res.end('500 - Internal Error');
+    } else {
+      res.writeHead(responseCode,
+        { 'Content-Type': contentType});
+      res.end(data);
+    }
+  })
+}
 
 http.createServer(function(req, res){
   // normalize url by removing querystring, optional
@@ -6,17 +21,17 @@ http.createServer(function(req, res){
   var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
   switch(path) {
     case '':
-      res.writeHead(200, { 'Content-Type': 'text/plain'});
-      res.end('Homepage');
+      serveStaticFile(res, '/public/home.html', 'text/html');
       break;
     case '/about':
-      res.writeHead(200, { 'Content-Type': 'text/plain'});
-      res.end('About');
+      serveStaticFile(res, '/public/about.html', 'text/html');
       break;
-    default:
-      res.writeHead(404, { 'Content-Type': 'text/plain'});
-      res.end('Not Found');
+    case '/img/logo.jpg':
+      serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
       break;    
+    default:
+      serveStaticFile(res, '/public/404.html', 'text/html', 404);
+      break;
   }
 }).listen(3000)
 
