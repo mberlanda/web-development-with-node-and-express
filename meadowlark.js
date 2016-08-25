f969d760d62b7044d45fe5c60d5feb7bb1e1ccf4
Mauro Berlanda <m.berlanda@fit2you.it>
2016-08-25 15:41:23 +0200

    ch08 Form Handling | Handling AJAX Forms

--- meadowlark.js
+++ meadowlark.js
@@ -92,12 +92,23 @@ app.get('/thank-you', function(req, res){
   res.render('thank-you');
 })
 
+/*
 app.post('/process', function(req, res){
   console.log('Form (from querystring): ' + req.query.form);
   console.log('CSRF token (from hidden form field): ' + req.body._csrf);
   console.log('Name (from visible form field): ' + req.body.name);
   console.log('Email (from visible form field): ' + req.body.email);
   res.redirect(303, '/thank-you');
+});
+*/
+app.post('/process', function(req, res){
+  if(req.xhr || req.accepts('json,html')==='json'){
+    // if there were an error, we would send { error: 'error description' }
+    res.send({ success: true });
+  } else {
+    // if there were an error, we would redirect to an error page
+    res.redirect(303, '/thank-you');
+  }
 });
 
 // custom 404 page
