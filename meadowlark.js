5665133bb36c980ae5246c2817f794bedc1662d7
Mauro Berlanda <m.berlanda@fit2you.it>
2016-08-26 15:45:24 +0200

    ch13 Persistence | Retrieving Data

--- meadowlark.js
+++ meadowlark.js
@@ -32,6 +32,58 @@ switch(app.get('env')){
   default:
     throw new Error('Unknown execution environment: ' + app.get('env'));
 }
+
+// Sending Initial Data
+Vacation.find(function(err, vacations){
+  if(err) return cosole.error(err);
+  if(vacations.length) return;
+  
+  new Vacation({
+    name: 'Hood River Day Trip',
+    slug: 'hood-river-day-trip',
+    category: 'Day Trip',
+    sku: 'HR199',
+    description: 'Spend a day sailing on the Columbia and ' +
+    'enjoying craft beers in Hood River!',
+    priceInCents: 9995,
+    tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
+    inSeason: true,
+    maximumGuests: 16,
+    available: true,
+    packagesSold: 0,
+  }).save();
+
+  new Vacation({
+    name: 'Oregon Coast Getaway',
+    slug: 'oregon-coast-getaway',
+    category: 'Weekend Getaway',
+    sku: 'OC39',
+    description: 'Enjoy the ocean air and quaint coastal towns!',
+    priceInCents: 269995,
+    tags: ['weekend getaway', 'oregon coast', 'beachcombing'],
+    inSeason: false,
+    maximumGuests: 8,
+    available: true,
+    packagesSold: 0,
+  }).save();
+
+  new Vacation({
+    name: 'Rock Climbing in Bend',
+    slug: 'rock-climbing-in-bend',
+    category: 'Adventure',
+    sku: 'B99',
+    description: 'Experience the thrill of climbing in the high desert.',
+    priceInCents: 289995,
+    tags: ['weekend getaway', 'bend', 'high desert', 'rock climbing'],
+    inSeason: true,
+    requiresWaiver: true,
+    maximumGuests: 4,
+    available: false,
+    packagesSold: 0,
+    notes: 'The tour guide is currently recovering from a skiing accident.',
+  }).save();
+});
+
 // filesystem persistence
 var fs = require('fs');
 
@@ -354,6 +406,31 @@ app.get('/epic-fail', function(req, res){
   });
 });
 
+// retrieving data
+app.get('/vacations', function(req, res){
+  Vacation.find({ available: true }, function(err, vacations){
+    /* bad implementation
+    var context = {
+      vacations: products.map(function(vacations){
+        vacation.price = vacation.getDisplayPrice();
+        return vacation;
+      });
+    };
+    */
+    var context = {
+      vacations: vacations.map(function(vacation){
+        return {
+          sku: vacation.sku,
+          name: vacation.name,
+          description: vacation.description,
+          price: vacation.getDisplayPrice(),
+          inSeason: vacation.inSeason,
+        }
+      })
+    };
+    res.render('vacations', context);
+  });
+});
 // custom 404 page
 app.use(function(req, res){
   res.status(404);
